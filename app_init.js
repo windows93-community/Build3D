6619abcb809f552b6e5f55df8578bff99c770fca
Fran <fran.rabljenovic12@gmail.com>
2020-05-27 13:12:59 +0200

    Updated app.html

---
+++ app_init.js
@@ -0,0 +1,8 @@
+le._apps["Build3D"] = {
+  name: "3DBuilder",
+  cathegories: "Graphics",
+  icon: "/c/files/images/icons/bomb.png",
+  exec: function() {
+    $exe("iframe /a/etc/3DBuilder/app.html --width=484 ---height=800 --icon=/c/files/images/icons/bomb.png");
+  }
+}
